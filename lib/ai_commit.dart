b3486c9492e9c1aabe41b9858cb8b9b6c9b3905d
Thit Lwin <thitlwincoder@gmail.com>
2023-11-22 22:19:04 +0630

    initial commit

---
+++ lib/ai_commit.dart
@@ -0,0 +1,10 @@
+/// ai_commit, A Very Good Project created by Very Good CLI.
+///
+/// ```sh
+/// # activate ai_commit
+/// dart pub global activate ai_commit
+///
+/// # see usage
+/// ai_commit --help
+/// ```
+library ai_commit;
